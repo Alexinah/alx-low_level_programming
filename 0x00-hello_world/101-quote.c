cb469820fe8f9f88301a1b1fc9a8b2ccd972e093
Alexinah <alexinamoraa2017@gmail.com>
2022-03-14 07:23:06 -0700

    Cprogram to print excat given characters without using printf, puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Result 1 (succes)
+ */
+int main(void)
+{
+	write(2,
+			
+"and thatpiece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
