d87b01c4025bfecfd2361fe6e9913aa8689bbb57
Alexinah <alexinamoraa2017@gmail.com>
2022-03-14 07:43:49 -0700

    Print exact quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Result: Always 1 (Success)
+ * Return: Aklways 1 (Success)
  */
 int main(void)
 {
-	write(2,"and thatpiece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece pf art is useful\" - Dora Korpar, 2015-10-19, 59);
 	return (1);
 }
