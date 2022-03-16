ef65213930045f0cbd24fe76873646fee9c25b12
Alexinah <alexinamoraa2017@gmail.com>
2022-03-16 13:52:05 +0000

    Program that prints the sum of fibonacci numbers below 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of fibonnacci numbers
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int cot;
+	unsigned int fib1 = 0, fib2 = 1, sum;
+
+	for (cot = 0; cot < 50; cot++)
+	{
+		sum = fib1 + fib2;
+		printf("%1u", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (cot == 49)
+			printf("\n");
+		else
+			printf(", ");
+
+	}
+	return (0);
+}
