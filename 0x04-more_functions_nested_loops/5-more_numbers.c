53226cc7c238059590eec467205c7e82c21160c2
Alexinah <alexinamoraa2017@gmail.com>
2022-03-17 19:53:01 +0000

    Function that prints numbers from 0-14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10x numbers from 0-14
+ *
+ * Return: void
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		_putchar('0');
+	}
+	for (j = 0; j <= 14; j++)
+	{
+		_putchar('j');
+	}
+	_putchar('\n');
+}
