9f1c236cea24d2ead6eff829d406ed28faa73932
Alexinah <alexinamoraa2017@gmail.com>
2022-03-29 12:40:01 +0000

    Function that return count of characters

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s : string to be counted
+ * @accept: The string containing the list of character to match s
+ * Return: Count
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	i = 0;
+	while (str1[i] != '\0')
+	{
+		j = 0;
+		while (str2[j] != '\0')
+		{
+			if (str2[j] == str1[i])
+			{
+				count++;
+				break;
+			}
+			j++;
+		}
+		if (s[i] != accept[j])
+		{
+			break;
+		}
+		i++;
+	}
+	return (count);
+}
